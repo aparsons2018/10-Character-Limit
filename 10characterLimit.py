d819dfab3783549ee9605e8106977c70d7c08347
Austin Parsons <parsons_aj@comcast.net>
2016-03-04 13:04:46 -0500

    changed characters in list to characters in lines.

--- 10characterLimit.py
+++ 10characterLimit.py
@@ -1,12 +1,12 @@
 #Austin Parsons
 import os
-f.open("input.txt") 
-
+f = open("input.txt") 
 lines = f.readlines()
-f = open("output.txt", w)
-for characters in list:
-	f.write(characters[1:10])
+f.close()
 
+f = open("output.txt", 'w')
+for characters in lines:
+	f.write(characters[0:10] + "\n")
 	
 
 f.close 
